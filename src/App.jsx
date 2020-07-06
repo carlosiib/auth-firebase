59800b5eb9b31167ba7595a00fdbb3706bb6250c
carlosiib <ntic.carlos@gmail.com>
2020-07-05 17:20:55 -0700

    Dependencies firebase and router

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="container">
-      <h1 className="text-center">Working</h1>
+      <h1 className="text-center">Auth Firebase</h1>
     </div>
   );
 }
