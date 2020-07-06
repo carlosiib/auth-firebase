14023cd48d2f450cc7848b867f052e271e2816b4
carlosiib <ntic.carlos@gmail.com>
2020-07-05 17:15:58 -0700

    Clean template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center">Working</h1>
+    </div>
+  );
+}
+
+export default App;
