decdcda3fa9f58fdf8054ea0acdcb66a898c792f
carlosiib <ntic.carlos@gmail.com>
2020-07-07 13:18:56 -0700

    Carga de usuario desde firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,8 +9,29 @@ import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Admin from "./components/Admin";
 
+import { auth } from "./firebase"
+
 function App() {
-  return (
+
+  const [firebaseUser, setFirebaseUser] = useState(false)
+
+  //Cargando usuario de firebase
+  useEffect(() => {
+    //detectando usuario actual en firebase
+    auth.onAuthStateChanged(user => {
+      console.log(user)
+      if (user) {
+        //usuario enviado state
+        setFirebaseUser(user)
+      } else {
+        //carga de usuario correcta, pasa de false a null
+        setFirebaseUser(null)
+      }
+    })
+  }, [])
+
+  //null !== false -> true
+  return firebaseUser !== false ? (
     <Router>
       <div className="container">
         <Navbar />
@@ -27,7 +48,9 @@ function App() {
         </Switch>
       </div>
     </Router>
-  );
+  ) : (
+      <p>Cargando...</p>
+    )
 }
 
 export default App;
